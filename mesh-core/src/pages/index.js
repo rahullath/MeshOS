e4b0fd62ecdf73be6a37d618b02754604ed57bfb
Rahul Lath <87206819+rahullath@users.noreply.github.com>
2025-04-20 08:06:58 +0530

    Update index.js

--- mesh-core/src/pages/index.js
+++ mesh-core/src/pages/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import React from 'react';
 import Head from 'next/head';
 import Layout from '../components/layout/Layout';
 import axios from 'axios';
