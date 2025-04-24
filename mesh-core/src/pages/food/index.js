9d463b5d45da786bfa692dcb8c59a2de1315c365
Rahul Lath <rahul.lath@science.christuniversity.in>
2025-04-24 23:18:54 +0530

    deploy error petcare fix

--- mesh-core/src/pages/food/index.js
+++ mesh-core/src/pages/food/index.js
@@ -2,14 +2,14 @@ import Layout from '../../components/layout/Layout';
 import Head from 'next/head';
 import MealPlanner from '../../components/food/MealPlanner';
 import GroceryInventory from '../../components/food/GroceryInventory';
-
+import PetCare from '../../components/food/PetCare';
 import PetSupplies from '../../components/food/PetSupplies';
 
 export default function FoodPage() {
   return (
     <Layout>
       <Head>
-        <title>Food | Mesh OS</title>
+        <title>Food | Life Dashboard</title>
       </Head>
       <div className="px-4 py-6">
         <h1 className="text-3xl font-bold text-gray-900">Food Dashboard</h1>
